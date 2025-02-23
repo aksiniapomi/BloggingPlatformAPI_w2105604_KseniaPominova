91843508cceb0ea3e865bc23581059d52b7fe6e8
aksiniapomi <kseniapominova@gmail.com>
2025-02-23 15:51:21 +0000

    LikeService.cs created

---
+++ Services/LikeService.cs
@@ -0,0 +1,65 @@
+using GothamPostBlogAPI.Data;
+using GothamPostBlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GothamPostBlogAPI.Services
+{
+    public class LikeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LikeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get all likes from the database wih User and BlogPost 
+        public async Task<List<Like>> GetAllLikesAsync()
+        {
+            return await _context.Likes
+                .Include(like => like.User)       //Include User who liked the post
+                .Include(like => like.BlogPost)   //Include the BlogPost that was liked
+                .ToListAsync();
+        }
+
+        //Get a like by ID
+        public async Task<Like?> GetLikeByIdAsync(int id)
+        {
+            return await _context.Likes
+                .Include(like => like.User)
+                .Include(like => like.BlogPost)
+                .FirstOrDefaultAsync(like => like.LikeId == id);
+        }
+
+        //Create a new like (User likes a BlogPost)
+        public async Task<Like> CreateLikeAsync(Like like)
+        {
+            //Check that a user cannot like the same post multiple times
+            var existingLike = await _context.Likes
+                .FirstOrDefaultAsync(like => like.UserId == like.UserId && like.BlogPostId == like.BlogPostId);
+
+            if (existingLike != null)
+            {
+                throw new InvalidOperationException("Error!User has already liked this post.");
+            }
+
+            _context.Likes.Add(like);
+            await _context.SaveChangesAsync();
+            return like;
+        }
+
+        //Remove a like (Unlike a post)
+        public async Task<bool> DeleteLikeAsync(int id)
+        {
+            var like = await _context.Likes.FindAsync(id);
+            if (like == null)
+            {
+                return false;
+            }
+
+            _context.Likes.Remove(like);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
--- obj/Debug/net9.0/BloggingPlatformAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/BloggingPlatformAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb306827c02867cd84af3cb7fa66a3cfda1b056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233258bf6691dd7864830c93860ffb52298afa19")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
