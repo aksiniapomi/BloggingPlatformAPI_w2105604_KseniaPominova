93b85963f9542c2760c016eeff7475f5c243f693
aksiniapomi <kseniapominova@gmail.com>
2025-02-26 12:32:15 +0000

    DTOs complete

--- Models/DTOs/BlogPostDTO.cs
+++ Models/DTOs/BlogPostDTO.cs
@@ -2,3 +2,19 @@
 //Improve security (e.g. prevent the user from setting the role)
 //Ensure validation (of the email format, fields filled)
 
+using System.ComponentModel.DataAnnotations;
+
+namespace GothamPostBlogAPI.Models.DTOs
+{
+    public class BlogPostDTO
+    {
+        [Required, MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }  //Prevent users from setting `UserId`
+    }
+}
--- Models/DTOs/CommentDTO.cs
+++ Models/DTOs/CommentDTO.cs
@@ -1,1 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace GothamPostBlogAPI.Models.DTOs
+{
+    public class CommentDTO
+    {
+        [Required]
+        public string CommentContent { get; set; }
+
+        [Required]
+        public int BlogPostId { get; set; }
+    }
+}
